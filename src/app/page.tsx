ba8270835b26dd1e5f95bda0bebe53b00d03c2d1
Teerothai Pranglaor <jedi@Teerothais-MacBook-Air.local>
2023-10-26 14:37:34 +0700

    feat: push related code to roomid for guy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,82 @@
 "use client";
 import Head from "next/head";
 import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState, useMemo } from "react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import { createContext } from "react";
 
 export const UserNameContext = createContext("asd");
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
+  const [roomId, setRoomId] = useState("");
   const router = useRouter();
 
-  const value = useMemo(() => ({ userInput, setUserInput }), [userInput]);
+  useEffect(() => {
+    fetch("http://localhost:8000/api/roomid/", { method: "GET" })
+      .then((res) => res.json())
+      .then((res) => {
+        setRoomId(res.room_id);
+      });
+  }, []);
 
   return (
     <>
-      <UserNameContext.Provider value={value.userInput}>
-        <Head>
-          <title>IQ 180</title>
-        </Head>
+      <Head>
+        <title>IQ 180</title>
+      </Head>
 
+      <Image
+        fill={true}
+        src="/background_iq180.svg"
+        alt="background image"
+        className="z-[-1]"
+      />
+      <div className="flex flex-col items-center justify-center z-[1] h-[100vh]">
         <Image
-          fill={true}
-          src="/background_iq180.svg"
+          src="/smarter-together.png"
+          width={400}
+          height={300}
           alt="background image"
-          className="z-[-1]"
+          className="border-solid border-gray-500 border-[1px]"
         />
-        <div className="flex flex-col items-center justify-center z-[1] h-[100vh]">
-          <Image
-            src="/smarter-together.png"
-            width={400}
-            height={300}
-            alt="background image"
-            className="border-solid border-gray-500 border-[1px]"
+        <div className="m-[25px] border-solid border-black border-[1px] rounded-lg flex flex-col justify-around items-center bg-[#DCDCDC] w-[300px] h-[150px]">
+          <input
+            maxLength={15}
+            type="text"
+            placeholder="Username"
+            className="w-[200px] h-[40px] text-black text-center border-solid border-black border-[1px]"
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+            value={userInput}
           />
-          <div className="m-[25px] border-solid border-black border-[1px] rounded-lg flex flex-col justify-around items-center bg-[#DCDCDC] w-[300px] h-[150px]">
-            <input
-              maxLength={15}
-              type="text"
-              placeholder="Username"
-              className="w-[200px] h-[40px] text-black text-center border-solid border-black border-[1px]"
-              onChange={(e) => {
-                setUserInput(e.target.value);
-              }}
-              value={userInput}
-            />
 
-            <div className="flex flex-col justify-between h-[60px]">
-              <button
-                className="text-white bg-[#FFBB54] w-[70px] border-transparent border-solid border-[1px] rounded-xl hover:transform hover:-translate-y-1 hover:shadow-md"
-                onClick={() => {
-                  router.push("/roomcreate");
-                }}
-              >
-                Create
-              </button>
-              <button
-                className="text-white bg-[#FFBB54] w-[70px] border-transparent border-solid border-[1px] rounded-xl hover:transform hover:-translate-y-1 hover:shadow-md"
-                onClick={() => router.push("/join-room")}
-              >
-                Join
-              </button>
-            </div>
+          <div className="flex flex-col justify-between h-[60px]">
+            <button
+              className="text-white bg-[#FFBB54] w-[70px] border-transparent border-solid border-[1px] rounded-xl hover:transform hover:-translate-y-1 hover:shadow-md"
+              onClick={() => {
+                localStorage.setItem("username", userInput);
+                localStorage.setItem("roomId", roomId);
+                router.push("/roomcreate");
+              }}
+              disabled={userInput == ""}
+            >
+              Create
+            </button>
+            <button
+              className="text-white bg-[#FFBB54] w-[70px] border-transparent border-solid border-[1px] rounded-xl hover:transform hover:-translate-y-1 hover:shadow-md"
+              onClick={() => {
+                localStorage.setItem("username", userInput);
+                router.push("/join-room");
+              }}
+              disabled={userInput == ""}
+            >
+              Join
+            </button>
           </div>
         </div>
-      </UserNameContext.Provider>
+      </div>
     </>
   );
 }
