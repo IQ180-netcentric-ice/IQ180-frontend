adfa06e37f0231e0cb5090c6320dc9a8cfae387f
guykrittin <krittin.siangprasertkij@outlook.com>
2023-11-05 19:55:11 +0700

    change to 4 garage ad , add tips

--- src/app/components/partial/AdBanner.tsx
+++ src/app/components/partial/AdBanner.tsx
@@ -9,30 +9,30 @@ const AdBanner = () => {
         bottom: 0,
         left: 0,
         width: "100%",
-        backgroundColor: "#f0f0f0", // Background color for the banner
+        backgroundColor: "#000000", // Background color for the banner
         textAlign: "center",
         zIndex: 9999, // Ensure it's above other content
       }}
-      className="flex flex-row"
+      className="flex flex-row justify-around"
     >
       <Image
-        src="/images/CasinoAdBanner.gif" // Replace with your GIF image path
+        src="/images/four-garage-ad.gif" // Replace with your GIF image path
         alt="Ad Banner"
         style={{ maxWidth: "100%", height: "auto" }}
-        width={700}
+        width={550}
         height={0}
         className="self-center"
       />
       <Image
-        src="/images/HuayBanner.gif" // Replace with your GIF image path
+        src="/images/four-garage-ad.gif" // Replace with your GIF image path
         alt="Ad Banner"
         style={{ maxWidth: "100%", height: "auto" }}
-        width={700}
+        width={550}
         height={0}
         className="self-center"
       />
       <Image
-        src="/images/CasinoAd2Banner.gif" // Replace with your GIF image path
+        src="/images/four-garage-ad.gif" // Replace with your GIF image path
         alt="Ad Banner"
         style={{ maxWidth: "100%", height: "auto" }}
         width={550}
--- src/app/components/partial/standby-box.tsx
+++ src/app/components/partial/standby-box.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
 interface StandbyBoxProps {
   text: string;
 }
 
 export default function StandbyBox({ text }: StandbyBoxProps): JSX.Element {
+  const tipsAndTricks = [
+    "Tip: Try to use all the available numbers to achieve the goal.",
+    "Tip: Use parentheses to control the order of operations.",
+    "Tip: Make sure to double-check your solution before submitting.",
+    "Tip: You can use addition, subtraction, multiplication, and division.",
+    "Tip: Calm mind always help you perform better.",
+    "Tip: Remember that it is just a game.",
+    // Add more tips and tricks here
+  ];
+
+  const [randomTip, setRandomTip] = useState("");
+
+  useEffect(() => {
+    // Select a random tip from the array
+    const randomIndex = Math.floor(Math.random() * tipsAndTricks.length);
+    setRandomTip(tipsAndTricks[randomIndex]);
+  }, []);
+
   return (
     <div className="flex flex-row items-center justify-center z-[1] h-[100vh]">
       <div className="m-[25px] p-5 border-solid border-black border-[1px] rounded-lg flex flex-col justify-center items-center bg-[#DCDCDC] w-[800px] h-[500px]">
@@ -16,15 +36,9 @@ export default function StandbyBox({ text }: StandbyBoxProps): JSX.Element {
         />
         <label className="text-2xl text-black font-bold">Waiting for</label>
         <label className="text-2xl text-blue-700 font-bold">Player2!</label>
-        <div className="m-[10px] flex p-1  w-[750px] h-[250px] rounded-lg">
-          <label className="text-black text-xl">
-            Once the time is out, please click continue button and you will be
-            sent to the results.
-          </label>
+        <div className="m-[10px] flex p-1 w-[750px] h-[250px] rounded-lg justify-center">
+          <label className="text-black text-xl">{randomTip}</label>
         </div>
-        <button className="text-white bg-blue-500 h-[100px] w-[200px] border-transparent border-solid border-[1px] rounded-xl hover:transform hover:-translate-y-1 hover:shadow-md">
-          Continue
-        </button>
       </div>
     </div>
   );
